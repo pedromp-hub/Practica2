b3e3bade307144bbf2b771e4d11cfce7c37d8362
pedromp-hub <pmonpel2304@g.educaand.es>
2025-10-23 11:53:05 +0200

    Segundo commit editando el archivo main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+//Cambio apartado 9 practica2
 public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
